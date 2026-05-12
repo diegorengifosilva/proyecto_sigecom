c2eaba2f2df71771777d9fc7af8fb41e8c967664
RonaldoRS30 <romansalvadorronaldoaldair@gmail.com>
2026-05-12 15:43:07 -0500

    ajustes

--- backend/settings.py
+++ backend/settings.py
@@ -87,7 +87,7 @@ pymysql.install_as_MySQLdb()
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": "proyecto_sigecom",
+        "NAME": "nueva_v2",
         "USER": "admin",
         "PASSWORD": "270509",
         "HOST": "127.0.0.1",
